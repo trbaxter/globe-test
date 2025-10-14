6c9d02aaabc860d7c6fa5ba85185a68087f81cd1
Tyler Baxter <trb7074@gmail.com>
2025-10-14 09:39:33 -0500

    Updated app.tsx with globe changes

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,20 +1,13 @@
 import Globe from '@/three/components/Globe';
 import SuperAreaFilter from '@/ui/SuperAreaFilter.tsx';
-import superAreas from '@/data/super_areas.json';
 import { useState } from 'react';
 
 export default function App() {
   const [filter, setFilter] = useState({ americas: false, apac: false, emea: false });
 
-  const highlightByRegion = {
-    americas: filter.americas ? superAreas.americas : [],
-    apac: filter.apac ? superAreas.apac : [],
-    emea: filter.emea ? superAreas.emea : []
-  };
-
   return (
     <div style={{ position: 'fixed', inset: 0 }}>
-      <Globe highlightByRegion={highlightByRegion} />
+      <Globe />
       <div style={{ position: 'fixed', top: 12, left: 12, zIndex: 10 }}>
         <SuperAreaFilter values={filter} onChange={setFilter} />
       </div>
