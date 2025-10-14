6d401f29ca939b294aaa8789e1d3dd7b3fa55752
Tyler Baxter <trb7074@gmail.com>
2025-10-14 14:58:28 -0500

    adding notes + testing higher pixel ratio

--- my-react-app/src/components/three/Globe.tsx
+++ my-react-app/src/components/three/Globe.tsx
@@ -3,47 +3,41 @@ import Globe, { type GlobeMethods } from 'react-globe.gl';
 import earthImg from '@/assets/img/earth-blue-marble.jpg';
 
 export default function GlobeComponent() {
-  // Mutable reference object. Object's .current value type either GlobeMethods or undefined.
-  // Undefined initial value on first render.
+  // Mutable reference object. Object's .current value type either GlobeMethods or undefined. Undefined initial value on first render.
   const globeRef = useRef<GlobeMethods | undefined>(undefined);
 
-  // Creates a state object initialized to current window size (if exists), otherwise (0,0).
-  // Deconstructs state to w & h variables. Uses setSize to update state and re-render.
+  // Initializes state object to current window size if exists. (0,0) otherwise. Deconstructs to w & h variables. Uses setSize to update state and re-render.
   const [{ w, h }, setSize] = useState({
     w: typeof window !== 'undefined' ? window.innerWidth : 0,
     h: typeof window !== 'undefined' ? window.innerHeight : 0
   });
 
+  // Listener hook to check if browser changes size and re-render the canvas to fit.
   useEffect(() => {
     const onResize = () => setSize({ w: window.innerWidth, h: window.innerHeight });
     window.addEventListener('resize', onResize);
     return () => window.removeEventListener('resize', onResize);
   }, []);
 
-  // mount-only setup
+  // Hook for setting up globe controls.
   useEffect(() => {
+    // If there's no current globe, don't continue with the hook.
     const globe = globeRef.current;
     if (!globe) return;
-    const controls = globe.controls?.();
-    controls?.saveState();
-    if (controls) {
-      const R = globe.getGlobeRadius?.() ?? 100;
-      controls.minDistance = R * 1.2;
-      controls.maxDistance = R * 2.3;
-      controls.update?.();
-    }
 
-    globe.pointOfView?.({ lat: 38, lng: -95, altitude: 0.75 }, 0);
+    globe?.renderer?.().setPixelRatio(2);
 
-    const mat = globe.globeMaterial?.();
-    if (mat) {
-      mat.polygonOffset = true;
-      mat.polygonOffsetFactor = 1;
-      mat.polygonOffsetUnits = 1;
-      mat.needsUpdate = true;
-    }
+    // If there's no controls object that exists, don't continue with the hook.
+    const c = globe.controls?.();
+    if (!c) return;
 
-    globe.renderer?.().setPixelRatio?.(Math.min(window.devicePixelRatio || 1, 3));
+    // Defines R = sphere radius value. Uses 100 if undefined. Sets min & max zoom levels.
+    const R = globe.getGlobeRadius?.() ?? 100;
+    c.minDistance = R * 1.2;
+    c.maxDistance = R * 3;
+
+    // Sets initial perspective on globe on initial render.
+    globe.pointOfView?.({ lat: 38, lng: -95, altitude: 1.75 }, 0);
   }, []);
 
   return (
