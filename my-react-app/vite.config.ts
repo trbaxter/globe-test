1a3273041605935f8ec2b8eccf33fbf12c9a3452
Tyler Baxter <trb7074@gmail.com>
2025-10-20 07:23:17 -0500

    removing unnecessary addition

--- my-react-app/vite.config.ts
+++ my-react-app/vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig(({ mode }) => ({
-  assetsInclude: ['**/*, .ktx2'],
   base: mode === 'production' ? '/globe-test/' : '/',
   plugins: [react()],
   resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } }
